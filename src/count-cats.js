49d7394c4edf6f9c7d8acd2c1e9c28d3dd52e7cb
Evgeniy Vasilko <shadezp@gmail.com>
2020-02-22 16:24:35 +0200

    feat: add cats and dreamTeam solutions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    // remove line with error and write your code here
+    let count = 0;
+    matrix.forEach(element => {
+      element.forEach(elem => { 
+        if (elem === "^^") {
+          count++;
+        }      
+      })
+    });
+    return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
   // remove line with error and write your code here
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  name = [];
+
+  members.forEach(element => {
+    if (typeof element === 'string') {
+      name.push(element.trim().toUpperCase()[0]);
+    };
+  });
+  return name.sort().join('')
 };

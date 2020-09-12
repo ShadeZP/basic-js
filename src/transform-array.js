a3988f58708d667e54034393e5535e2d7ab7751e
ShadeZP <ShadeZP@gmail.com>
2020-09-13 01:33:31 +0300

    fix: fix transofrm-array bug

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,8 +7,9 @@ module.exports = function transform(arr ) {
     for (let i=0; i < arr.length; i++) {
         if (arr[i] == '--discard-next') {
             i++
-        }else if (arr[i] == '--discard-prev' && arr[i - 2] !== '--discard-next'){            
-            result.pop();
+        }else if (arr[i] == '--discard-prev' && arr[i - 2] == '--discard-next'){ //do nothing           
+        }else if (arr[i] == '--discard-prev'){
+            result.pop();            
         }else if (arr[i]=='--double-next'){
             if (i<arr.length-1){
                 result.push(arr[i+1]);
@@ -17,7 +18,6 @@ module.exports = function transform(arr ) {
             if (i > 1 && arr[i - 2] !== '--discard-next'){
                 result.push(arr[i-1]);
             };
-        }else if (arr[i] == '--discard-prev'){
         }else{
             result.push(arr[i]);
         };

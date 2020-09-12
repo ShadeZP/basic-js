fa8ce77206a8d8a0d6b6ea6ce9127e603ff72f02
ShadeZP <ShadeZP@gmail.com>
2020-09-13 01:31:41 +0300

    feat: add hanoi-tower & transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,19 +4,20 @@ module.exports = function transform(arr ) {
         throw new Error ();
     };
 
-    for (i=0; i<arr.length; i++) {
-        if (arr[i]=='--discard-next') {
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
             i++
-        }else if (arr[i]=='--discard-prev'){
+        }else if (arr[i] == '--discard-prev' && arr[i - 2] !== '--discard-next'){            
             result.pop();
         }else if (arr[i]=='--double-next'){
             if (i<arr.length-1){
                 result.push(arr[i+1]);
             };
-        }else if (arr[i]=='--double-prev'){
-            if (i>1){
+        }else if (arr[i] == '--double-prev'){
+            if (i > 1 && arr[i - 2] !== '--discard-next'){
                 result.push(arr[i-1]);
             };
+        }else if (arr[i] == '--discard-prev'){
         }else{
             result.push(arr[i]);
         };
